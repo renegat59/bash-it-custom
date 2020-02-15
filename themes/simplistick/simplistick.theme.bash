7bb0c8dd48cdbc130db589f08195f63abbe77553
Mateusz P <bananq@gmail.com>
2020-02-15 15:50:13 +0100

    added aliases for git

--- themes/simplistick/simplistick.theme.bash
+++ themes/simplistick/simplistick.theme.bash
@@ -7,25 +7,25 @@ SCM_THEME_PROMPT_DIRTY=" ${red}✗"
 SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
 SCM_THEME_PROMPT_PREFIX="("
 SCM_THEME_PROMPT_SUFFIX="${green}) "
-SCM_GIT_AHEAD_CHAR="${green}+"
-SCM_GIT_BEHIND_CHAR="${red}-"
+SCM_GIT_AHEAD_CHAR="${green}▲"
+SCM_GIT_BEHIND_CHAR="${red}▼"
 
 GIT_THEME_PROMPT_DIRTY=" ${bold_red}✗"
-GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓👍"
-GIT_THEME_PROMPT_PREFIX="${cyan}("
-GIT_THEME_PROMPT_SUFFIX="${cyan}) "
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX="${white}("
+GIT_THEME_PROMPT_SUFFIX="${white}) "
 
-RVM_THEME_PROMPT_PREFIX="|"
-RVM_THEME_PROMPT_SUFFIX="| "
+RVM_THEME_PROMPT_PREFIX="("
+RVM_THEME_PROMPT_SUFFIX=") "
 
-VIRTUALENV_THEME_PROMPT_PREFIX="|"
-VIRTUALENV_THEME_PROMPT_SUFFIX="| "
+VIRTUALENV_THEME_PROMPT_PREFIX="("
+VIRTUALENV_THEME_PROMPT_SUFFIX=") "
 
-RBENV_THEME_PROMPT_PREFIX="|"
-RBENV_THEME_PROMPT_SUFFIX="| "
+RBENV_THEME_PROMPT_PREFIX="("
+RBENV_THEME_PROMPT_SUFFIX=") "
 
-RBFU_THEME_PROMPT_PREFIX="|"
-RBFU_THEME_PROMPT_SUFFIX="| "
+RBFU_THEME_PROMPT_PREFIX="("
+RBFU_THEME_PROMPT_SUFFIX=") "
 
 function rvm_version_prompt {
   if which rvm &> /dev/null; then
@@ -37,23 +37,14 @@ function rvm_version_prompt {
 
 function git_prompt_info {
   git_prompt_vars
-  echo -e "$SCM_PREFIX$SCM_BRANCH$SCM_STATE$SCM_GIT_AHEAD$SCM_GIT_BEHIND$SCM_GIT_STASH$SCM_SUFFIX"
+  echo -e "$SCM_PREFIX${yellow}$SCM_BRANCH$SCM_STATE$SCM_GIT_AHEAD$SCM_GIT_BEHIND$SCM_GIT_STASH$SCM_SUFFIX"
 }
 
 LAST_PROMPT=""
 function prompt_command() {
-    local new_PS1="${bold_cyan}$(scm_char)${yellow}$(ruby_version_prompt)${green}\w $(scm_prompt_info)"
-    local new_prompt=$(PS1="$new_PS1" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
-
-    if [ "$LAST_PROMPT" = "$new_prompt" ]; then
-        new_PS1=""
-    else
-        LAST_PROMPT="$new_prompt"
-    fi
-
-    local wrap_char=""
-    [[ $COLUMNS && ${#new_PS1} > $(($COLUMNS/1)) ]] && wrap_char="\n"
-    PS1="${new_PS1}${green}${wrap_char}→${reset_color} "
+    local new_PS1="${white}> $(scm_prompt_info)${bold_cyan}$(scm_char)${yellow}$(ruby_version_prompt)${green}\w"
+    local wrap_char="\n"
+    PS1="${new_PS1}${green}${wrap_char}%${reset_color} "
 }
 
 safe_append_prompt_command prompt_command
